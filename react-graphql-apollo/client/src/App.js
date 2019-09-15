05b2b3af9db0d5d12d83a360fc0cf2913c8156b7
Aashis Rimal <aashis_rimal@yahoo.com>
2019-09-15 10:14:32 +0300

    Installed apollo-boost, @apollo/react-hooks and graphql and used in App.js to create a ApolloClient and pass it to the react app

--- react-graphql-apollo/client/src/App.js
+++ react-graphql-apollo/client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+// ApolloClient needs to be imported as below, no combination to avoid errors
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import './App.css';
 import './css/cyborg-bootstrap.min.css';
+import logo from './logo.png';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:5000/graphql'
+});
 
 function App() {
     return (
-        <div className='App'>
-            <h1>SpaceX</h1>
-        </div>
+        <ApolloProvider client={client}>
+            <div className='container'>
+                <img src={logo} alt='SpaceX' style={{ width: 300, display: 'block', margin: 'auto' }}></img>
+            </div>
+        </ApolloProvider>
     );
 }
 
